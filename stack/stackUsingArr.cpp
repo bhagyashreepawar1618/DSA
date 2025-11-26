cf7be21a4942b5c6c28908df8662ea690780c2df
bhagyashreepawar1618 <bhagyashreepawar1618@gmail.com>
2025-11-27 00:18:40 +0530

    implementation of stack using linked list

--- stack/stackUsingArr.cpp
+++ stack/stackUsingArr.cpp
@@ -5,8 +5,7 @@ using namespace std;
 class stImplarr
 {
     public:
-    int s=10;
-    int st[s];
+    int st[10];
     int top=-1;
 
     //push operation
@@ -22,7 +21,68 @@ class stImplarr
         {
             top=top+1;
             st[top]=x;
+            cout<<"element added successfully..."<<endl;
         }
     }
+
+    void pop()
+    {
+        if(top==-1)
+        {
+            cout<<"underflow..."<<endl;
+        }
+        else
+        {
+            top=top-1;
+            cout<<"element popped.."<<endl;
+        }
+    }
+
+    int Top()
+    {
+        if(top==-1)
+        {
+            cout<<"empty"<<endl;
+            return -1;
+        }
+        else
+        {
+            return st[top];
+        }
+    }
+
+    int size()
+    {
+        return top+1;
+    }
+
+    void display()
+    {
+        for(int i=0;i<=top;i++)
+        {
+            cout<<st[i]<<" ";
+        }
+        cout<<endl;
+    }
+};
+
+int main()
+{
+    stImplarr s;
+    s.push(10);
+    s.push(20);
+    s.push(30);
+    s.push(100);
+    s.display();
+    s.pop();
+    s.display();
+    s.pop();
+    s.display();
+    cout<<"size of the stack is="<<s.size()<<endl;
+    cout<<"top element is"<<s.Top()<<endl;
+    s.push(150);
+    s.display();
+    return 0;
 }
+
 
